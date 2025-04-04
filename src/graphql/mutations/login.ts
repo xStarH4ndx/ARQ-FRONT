32f82a7a162641f6bdfaa6b68bdde312092b8f51
xStarH4ndx <brunotoro1799@gmail.com>
2025-04-04 18:39:12 -0300

    ajustes añadidos

--- src/graphql/mutations/login.ts
+++ src/graphql/mutations/login.ts
@@ -1,12 +1,13 @@
 import { gql } from '@apollo/client'
 
-export const LOGIN_ADMIN = gql`
-  mutation LoginAdmin($email: String!, $password: String!) {
-    loginAdmin(email: $email, password: $password) {
-      access_token
-    }
-  }
-`
+// NO HAY ADMINISTRADORES EN LA BASE DE DATOS
+// export const LOGIN_ADMIN = gql`
+//   mutation LoginAdmin($email: String!, $password: String!) {
+//     loginAdmin(email: $email, password: $password) {
+//       access_token
+//     }
+//   }
+// `
 
 export const LOGIN_PERSONAL = gql`
   mutation LoginPersonal($email: String!, $password: String!) {
